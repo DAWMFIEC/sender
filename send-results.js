96b3acb63957461bd3fad0946ec21bae842bc52f
Allan <aavendan@gmail.com>
2025-05-03 11:13:28 -0500

    fs, path, execSync

--- send-results.js
+++ send-results.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
 
 async function sendResults( nameStudent, codeExercise ) {
 
